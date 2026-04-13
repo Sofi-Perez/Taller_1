c5c95b61a563350903c86cf388fbca9c9a8640c2
Sofi-Perez <s.perezr234@uniandes.edu.co>
2026-04-13 04:36:03 +0000

    Taller 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Taller_1
+Sofia Perez -202410682
---
+++ main.ts
@@ -0,0 +1,26 @@
+import { series } from './data.js';
+import { Serie } from './serie.js';
+
+const seriesTable: HTMLElement = document.getElementById("series-body")!;
+const averageSeasonsText: HTMLElement = document.getElementById("average-seasons")!;
+
+renderSeriesTable(series);
+averageSeasonsText.innerHTML = `Seasons average: ${getAverageSeasons(series)}`;
+
+function renderSeriesTable(seriesList: Serie[]): void {
+  seriesList.forEach((serie) => {
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `
+        <td><b>${serie.id}</b></td>
+        <td style="color: #007bff;">${serie.name}</td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>`;
+    seriesTable.appendChild(trElement);
+  });
+}
+
+function getAverageSeasons(seriesList: Serie[]): number {
+  let totalSeasons: number = 0;
+  seriesList.forEach((s) => totalSeasons += s.seasons);
+  return totalSeasons / seriesList.length;
+}
